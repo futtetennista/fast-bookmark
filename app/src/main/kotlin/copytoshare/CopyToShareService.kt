1b186c2466bcba2c3e778f6652856ee346325bb8
Stefano Dacchille <stefano.dacchille@gmail.com>
2015-11-21 16:41:28 +0100

    [WIP] Share link directly to favourite app

--- app/src/main/kotlin/copytoshare/CopyToShareService.kt
+++ app/src/main/kotlin/copytoshare/CopyToShareService.kt
@@ -3,33 +3,72 @@ package copytoshare
 import android.app.Service
 import android.content.ClipboardManager
 import android.content.Intent
-import android.net.Uri
+import android.content.pm.PackageManager.MATCH_DEFAULT_ONLY
 import android.os.IBinder
 import playground.copytoshare.R
 import java.net.MalformedURLException
 import java.net.URL
 
-class CopyToShareService: Service(), ClipboardManager.OnPrimaryClipChangedListener {
+class CopyToShareService : Service(), ClipboardManager.OnPrimaryClipChangedListener {
 
   override fun onPrimaryClipChanged() {
     if (!clipboardManager.hasPrimaryClip()) return
 
     val item = clipboardManager.primaryClip.getItemAt(0)
     if (isUrl(item.text.toString())) {
-      applicationContext.startActivity(createShareIntent(item.text))
+      val baseIntent = Intent(Intent.ACTION_SEND)
+          .setType("text/plain")
+          .putExtra(Intent.EXTRA_TEXT, item.text)
+      val intent = favouriteBookmarksAppOrChooserIntent(baseIntent)
+      if (intent != null) {
+        applicationContext.startActivity(intent)
+      }
     }
   }
 
-  private fun createShareIntent(link: CharSequence?): Intent? {
-    val intent = Intent(Intent.ACTION_SEND)
-        .setType("text/plain")
-        .putExtra(Intent.EXTRA_TEXT, link)
-    val chooserIntent =
-        Intent.createChooser(intent, applicationContext.getString(R.string.save_link))
-    chooserIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-    return chooserIntent
+  private fun favouriteBookmarksAppOrChooserIntent(intent: Intent): Intent? {
+    val info = retrieveFavouriteBookmarksAppInfo(intent)
+    if (info != null) {
+      return Intent(intent).setClassName(info.first, info.second)
+          .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    } else {
+      val chooserIntent =
+          Intent.createChooser(intent, applicationContext.getString(R.string.save_link))
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+      return if (chooserIntent.resolveActivity(packageManager) != null) {
+        chooserIntent
+      } else {
+        null
+      }
+    }
   }
 
+  private fun retrieveFavouriteBookmarksAppInfo(intent: Intent): Pair<String, String>? {
+    val info = retrieveStoredFavouriteBookmarksAppInfo()
+    return if (favouriteBookmarksAppInstalled(intent, info)) {
+      info
+    } else {
+      null
+    }
+  }
+
+  private fun favouriteBookmarksAppInstalled(intent: Intent,
+                                             shareActivityInfo: Pair<String, String>)
+      : Boolean {
+    val activities =
+        applicationContext.packageManager.queryIntentActivities(intent, MATCH_DEFAULT_ONLY)
+    val res = activities.filter { a ->
+      a.activityInfo.packageName == shareActivityInfo.first
+          && a.activityInfo.name == shareActivityInfo.second
+    }
+    return res.isNotEmpty()
+  }
+
+  private fun retrieveStoredFavouriteBookmarksAppInfo(): Pair<String, String> {
+    return Pair("com.ideashower.readitlater.pro", "com.ideashower.readitlater.activity.AddActivity")
+  }
+
+  // TODO: is there a cleaner way to do this that doesn't involve regex?
   private fun isUrl(text: String?): Boolean {
     try {
       URL(text)
